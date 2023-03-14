9e67be5d7622243bbca693f74e52b3bc1f60c52f
Ing. Jose Dumar Jimenez Ruiz <nowen21@gmail.com>
2023-03-13 20:10:15 -0500

    tutorial # 4 de angular

--- src/app/layouts/Iportafolio/components/presentacion/presentacion.component.ts
+++ src/app/layouts/Iportafolio/components/presentacion/presentacion.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-presentacion',
-  templateUrl: './presentacion.component.html',
+  templateUrl: './tutorial.component.html',
   styleUrls: ['./presentacion.component.css']
 })
 export class PresentacionComponent implements OnInit {
